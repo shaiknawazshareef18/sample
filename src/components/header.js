08ec24b9f354b7692cbfa9ce6bdf186ee907f598
renzralliongomez <44893317+renzralliongomez@users.noreply.github.com>
2021-06-18 13:07:42 +0800

    1

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,44 @@
 import React from "react";
 import {withRouter, Link} from "react-router-dom";
-
+import LogoS from "../assets/logo_s.png"
 import {AppBar, Toolbar, Typography, Button, makeStyles, Dialog, TextField, DialogContent, DialogActions} from '@material-ui/core';
 import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+<<<<<<< Updated upstream
 
 import {useState, useEffect} from 'react';
 import { authentication } from "../firebase";
+=======
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {useState} from 'react';
+>>>>>>> Stashed changes
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#D99818',
+        },
+        secondary: {
+            accent: '#898C26',
+            main: '#FFF1DC',
+            contrastText: '#111111'
+        },
+        contrastThreshold: 3,
+        tonalOffset:0.2,
+    },
+});
 
 const styles = makeStyles( (theme) => ({
     title: {
         flexGrow: 1,
     },
+    logo: {
+        maxWidth: 32,
+        margin: 8,
+    },
 }));
+
+
 
 const Header = props => {
 
@@ -67,17 +93,21 @@ const Header = props => {
     });
 
     return (
-        <>  
-            <AppBar position="sticky">
+        <> 
+        <ThemeProvider theme={theme}>
+            <AppBar position="sticky" color="secondary">
             <Toolbar>
-                <Typography 
+                <>
+                    <img src = {LogoS} alt="logo" className={classes.logo}/>
+                    <Typography 
                     style={{cursor: "pointer"}}
                     className={classes.title}
                     variant="h6"
                     onClick={()=>history.push('/')}
-                >
-                    Indigenous Artifacts Hub
-                </Typography>
+                    >
+                        Indigenous Artifacts Hub
+                    </Typography>
+                </>
                 <Button 
                     color="inherit"
                     startIcon={<InfoRoundedIcon />}
@@ -148,6 +178,7 @@ const Header = props => {
                     </Button>
                 </DialogActions>
             </Dialog>
+            </ThemeProvider>
         </>
     );
 }
